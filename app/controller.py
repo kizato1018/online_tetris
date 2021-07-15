6d4bdc2cb79066d793928b58a9bde39402b6615e
eoleedi <eoleedimin@gmail.com>
2021-07-16 02:14:33 +0800

    2021/7/16 keyboard debug

--- app/controller.py
+++ app/controller.py
@@ -3,43 +3,77 @@ import tkinter as tk
 import logging
 import time
 from pynput import keyboard
+import threading
 
 from models import Model
 from views import View
 from objects import Block
 
+import logconfig
 
 
 class Controller():
     def __init__(self) -> None:
+
         self.root = tk.Tk()
         self.model = Model()
         self.view = View(self.root, self)
         self.root.title("Online tetris")
         self.root.resizable(TRUE, TRUE)
         self.root.minsize(width=500, height=300)
+        self.init_keyboard_controls()
+        self.keydown = False
         self.root.mainloop()
-
-    
 
     def initialize_game(self, gamemode: str):
         tableWidth = 10
         tableHeight = 20
         logging.info("Game initializing...")
+        # init game GUI
         self.show_frame(f'{gamemode}Page')
-        self.view.initialize_gametable(tableWidth, tableHeight)
+
+        # init gametable
+        self.view.frame.initialize_gametable(tableWidth, tableHeight)
+
+        # init game control key
+        self.controls['key.left'] = self.model.action_left
+        self.controls['key.right'] = self.model.action_right
+        #self.controls['key.up'] = self.model.action_up
+        self.controls['key.down'] = self.model.action_down
+
         self.root.update()
-        self.init_keyboard_controls()
+
         self.start_game(gamemode)
 
+    def on_press(self, keyname):
+        """handler for keyboard listener"""
+        if self.keydown:
+            return
+        try:
+            self.keydown = True
+            keyname = str(keyname).strip('\'')
+            logging.debug('+' + keyname)
+            if keyname == 'Key.esc':
+                pass
+            if keyname in self.controls:
+                key_handler = self.controls[keyname]
+                key_handler()
+        except AttributeError:
+            logging.error('special key {0} pressed'.format(keyname))
+
+    def on_release(self, keyname):
+        """Reset on key up from keyboard listener"""
+        self.keydown = False
+        keyname = str(keyname).strip('\'')
+        logging.debug('-' + keyname)
+        if keyname in self.controls:
+            key_handler = self.controls[keyname]
+            key_handler()
+
     def init_keyboard_controls(self):
-        self.controls = {
-            'Key.left':  self.model.action_left,
-            'Key.right': self.model.action_right,
-            'Key.up': self.model.action_up,
-            'Key.down': self.model.action_down,
-        }
-        self.key_listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
+        self.controls = dict()
+        self.key_listener = keyboard.Listener(on_press=self.on_press,
+                                              on_release=self.on_release)
         self.key_listener.start()
 
     def show_frame(self, page_name):
@@ -55,8 +89,6 @@ class Controller():
 
     def single_mode_game():
         pass
-    
+
     def multi_mode_game():
         pass
-
-    
